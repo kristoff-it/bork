9c837eb19bc60b65410bff7b5e818b2d9c4414e8
Loris Cro <kappaloris@gmail.com>
2020-12-27 02:54:01 +0100

    forgot deps

---
+++ deps.zig
@@ -0,0 +1,22 @@
+pub const pkgs = .{
+    .zbox = .{
+        .name = "zbox",
+        .path = "forks/zbox/src/box.zig",
+    },
+    .bearssl = .{
+        .name = "bearssl",
+        .path = "forks/BearSSL/bearssl.zig",
+    },
+    .datetime = .{
+        .name = "datetime",
+        .path = "zig-deps/68582870b79584c72d1164716da5bd48/datetime.zig",
+    },
+    .mecha = .{
+        .name = "mecha",
+        .path = "zig-deps/3a3ba987887772ca8d8d94a1275c84b7/mecha.zig",
+    },
+    .hzzp = .{
+        .name = "hzzp",
+        .path = "zig-deps/8118837137479ba87bb998468dc4159d/src/main.zig",
+    },
+};
