06323f29af1ba79f6311eaf8737190cd013bddd5
Meghan Denny <hello@nektro.net>
2021-08-16 21:40:59 -0700

    cleanup build.zig

--- build.zig
+++ build.zig
@@ -1,17 +1,9 @@
 const std = @import("std");
-const Builder = std.build.Builder;
-const Pkg = std.build.Pkg;
 const pkgs = @import("deps.zig").pkgs;
 
-pub fn build(b: *Builder) void {
-    // Standard target options alloirc the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("bork", "src/main.zig");
     pkgs.addAllTo(exe);
