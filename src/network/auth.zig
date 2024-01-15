f99d759b61d257a51e7baccfb42fc3c9c9448768
Loris Cro <kappaloris@gmail.com>
2024-01-15 15:56:46 +0100

    update Bork to latest Zig

--- src/network/auth.zig
+++ src/network/auth.zig
@@ -6,6 +6,37 @@ const url = "https://id.twitch.tv/oauth2/validate";
 pub fn checkTokenValidity(gpa: std.mem.Allocator, token: []const u8) !bool {
     if (build_opts.local) return true;
 
+    var it = std.mem.tokenize(u8, token, ":");
+    _ = it.next();
+    const header_oauth = try std.fmt.allocPrint(gpa, "Authorization: OAuth {s}", .{it.next().?});
+    defer gpa.free(header_oauth);
+
+    const result = try std.ChildProcess.run(.{
+        .allocator = gpa,
+        .argv = &.{
+            "curl",
+            "-s",
+            "-o",
+            "/dev/null",
+            "-w",
+            "%{http_code}",
+            "-H",
+            header_oauth,
+            url,
+        },
+    });
+
+    defer {
+        gpa.free(result.stdout);
+        gpa.free(result.stderr);
+    }
+
+    return std.mem.eql(u8, result.stdout, "200");
+}
+
+pub fn checkTokenValidityNative(gpa: std.mem.Allocator, token: []const u8) !bool {
+    if (build_opts.local) return true;
+
     var client: std.http.Client = .{
         .allocator = gpa,
     };
